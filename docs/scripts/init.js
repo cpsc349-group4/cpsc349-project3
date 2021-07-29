ce72182c88ee65d44b689355da6b7753cc8202ba
David Bonnaud <dbonnaud123@gmail.com>
2021-07-29 02:31:39 -0700

    cleaned up html, created exposed login and post functionality with js, displayed public timeline data with js, styled everything with tailwind

--- docs/scripts/init.js
+++ docs/scripts/init.js
@@ -1,7 +1,33 @@
 import * as mockroblog from './mockroblog.js'
-window.mockroblog = mockroblog
+window.mockroblog = mockroblog;
 
-const timeline = mockroblog.getPublicTimeline()
+//display public timeline
+const publicTimeline = mockroblog.getPublicTimeline();
+const publicDisplay = document.querySelector('#publicTimeline-json');
 
-const display = document.querySelector('#timeline-json')
-display.textContent = JSON.stringify(timeline, null, 2)
+publicTimeline.forEach(post => {
+    publicDisplay.innerHTML += `<article class="post"><div class="userId">User: ${post.id}</div><div class="postText">${post.text}</div><div class="postTimestamp">${post.timestamp}</div></article>`;
+});
+
+//authenticate user data from login form on login button click
+if(document.getElementById('loginButton')){
+    const loginButton = document.getElementById('loginButton');
+    loginButton.onclick = () => {
+        let username = document.getElementById('username').value;
+        let password = document.getElementById('password').value;
+        let data = mockroblog.authenticateUser(username, password);
+        console.log(data);
+    }
+}
+
+//create a new post
+if(document.getElementById('newPostButton')){
+    const newPostButton = document.getElementById('newPostButton');
+    newPostButton.onclick = () => {
+        let postText = document.getElementById('PostText').value;
+        let data = mockroblog.postMessage(4, postText)
+        console.log(data);
+    }
+}
+
+
